49cc4c7df0afa773be67e9813464cc9cc11979ff
Hendri Korasi Simbolon <hendrikorasisimbolon@gmail.com>
2017-05-22 11:12:51 +0700

    Change Cover Presentation and Theme

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,6 +23,7 @@ bespoke.from('article', [
   forms()
 ]);
 
+
 // Prism syntax highlighting
 // This is actually loaded from "bower_components" thanks to
 // debowerify: https://github.com/eugeneware/debowerify
